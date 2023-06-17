7b9e076b369697a1d8b0f71ddbb3f24095f4be56
Ferenc Bakonyi <ferenc.bakonyi@balabit.com>
2023-06-17 06:46:49 +0200

    Adjust PIDs and filament load/unload parameters.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -622,9 +622,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp 16.96
-    #define DEFAULT_Ki 0.84
-    #define DEFAULT_Kd 85.79
+    #define DEFAULT_Kp 26.92
+    #define DEFAULT_Ki 2.24
+    #define DEFAULT_Kd 81.06
   #endif
 #endif
 
@@ -705,9 +705,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 79.24
-  #define DEFAULT_bedKi 6.67
-  #define DEFAULT_bedKd 627.54
+  #define DEFAULT_bedKp 115.46
+  #define DEFAULT_bedKi 12.05
+  #define DEFAULT_bedKd 737.77
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
