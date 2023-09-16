7d1958a008387baa6573cf442e4e3c69334feaf9
Ferenc Bakonyi <ferenc.bakonyi@balabit.com>
2023-09-16 22:43:43 +0200

    Add PETG presets

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2073,6 +2073,12 @@
 #define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
+#define PREHEAT_3_LABEL       "PETG"
+#define PREHEAT_3_TEMP_HOTEND 210
+#define PREHEAT_3_TEMP_BED     70
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
+
 /**
  * Nozzle Park
  *
