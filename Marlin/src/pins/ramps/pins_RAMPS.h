7277cefe9929b25b30d78a735d12c2d4c4acb3d4
Ferenc Bakonyi <bakonyi.ferenc@gmail.com>
2024-02-18 09:06:26 +0100

    Fix Z_MIN_PROBE_PIN name (new name convention)

--- Marlin/src/pins/ramps/pins_RAMPS.h
+++ Marlin/src/pins/ramps/pins_RAMPS.h
@@ -136,7 +136,7 @@
 // Z Probe (when not Z_MIN_PIN)
 //
 #ifndef Z_MIN_PROBE_PIN
-  #define Z_MIN_PROBE_PIN                     AUX1_05_PIN
+  #define Z_MIN_PROBE_PIN                     AUX1_05
 #endif
 
 //
